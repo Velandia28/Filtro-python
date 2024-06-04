f9dd4f0f98c5fb5ff93c81aca8c9d012a6f294a1
Velandia28 <Luisfelipevll28@gmail.com>
2024-06-04 13:19:25 -0500

    ......

--- proyecto filtro python/main.py
+++ proyecto filtro python/main.py
@@ -1,19 +1,26 @@
 print ("HOLA BIENVENIDO A CAMPUSLANDS")
-from mod_registrar import *
+from mod_registrar import * 
 import json
+
 while True:
 
-    menu_principal=("1. Regristrar nuevos Campers","2. Rutas","3. Areas de Entrenamiento","4. Coordinacion Academica","5. Trainers","6. SALIR")
+    menu_principal=("1. registrar coordinador ","2.","3. Areas de Entrenamiento","4. Coordinacion Academica","5. Trainers","6. SALIR")
     for i in menu_principal:
         print(i)
-    opc = (int(input("Ingrese el numero del menu al qu1e desea ingresar: ")))
+    opc = (int(input("Ingrese el numero del menu al que desea ingresar: ")))
     if opc == len(menu_principal):
         print("SALIENDO.....")
         break
+
     elif opc == 1:
         print("********************************************************")
-        print("Haz seleccionado la opcion de Registrar un nuevo camper ")
+        print("Haz seleccionado la opcion de Registrar como coordinador un nuevo camper ")
         registrar_camper()
+        print("********************************************************")
+    elif opc == 2:
+        print("********************************************************")
+        print("Haz seleccionado la opcion de Rutas ")
+        nueva_ruta()
         print("********************************************************")
         
 
--- proyecto filtro python/mod_registrar.py
+++ proyecto filtro python/mod_registrar.py
@@ -1,17 +1,15 @@
 import json 
-
+ruta_archivo = "proyecto filtro python/campers.json"
 def lee_json():
     try:
-        with open("campers.json","r")as file:
+        with open("proyecto filtro python/campers.json","r")as file:
             contenido = file.read()
             contenido = json.loads(contenido)
             return contenido
     except FileNotFoundError:
         print("El archivo no existe")
+        print("¡¡¡ Creando Archivo  !!")
         return []    
-
-
-ruta_archivo = "campers.json"
 
 def registrar_camper(): 
     data=lee_json()
@@ -26,20 +24,25 @@ def registrar_camper():
     "Ruta Asignada" : "",
     "Riesgo":""
     }
-
     datos["Doc"]=int(input("Ingrese su Número de Documento : "))
     datos["Nombres"]=(input("Ingrese sus Nombres: "))
     datos["Apellidos"]=(input("Ingrese sus Apellidos: "))
     datos["Direccion"]=(input("Ingrese su Direccion de residencia: "))
     datos["Acudiente"]=(input("Ingrese los Datos de su Acudiente (Nombre completo,Parentesco y Número telefonico):  "))
     datos["Celular"]=(int(input("Ingrese su Número de celular:  ")))
+    datos["Ruta Asignada"]=(input("Ingrese la ruta que va a desarrollar el camper (NodeJs, Java, Netcore):  "))
     data.append(datos)
 
     with open(ruta_archivo,"w") as file:
         file.write(json.dumps(data, indent=4))
+        print("********************************************************")
         print("Se ha registrado correctamente")
+        print("********************************************************")
         print(data)
     return data
 
-    
-        
+def nueva_ruta():
+    with open(ruta_archivo, 'r') as file:
+        datos = json.load('campers.json')
+        datos["Ruta Asignada"]=input("seleccione la ruta que va a desarrollar (NodeJs, Java, Netcore): ")
+        json.close()
